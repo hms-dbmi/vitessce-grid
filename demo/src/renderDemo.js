a683a11d931c265a9a2b6ff5741b17525c2818cb
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-05-14 09:52:18 -0400

    Move config to json

--- demo/src/renderDemo.js
+++ demo/src/renderDemo.js
@@ -8,6 +8,8 @@ import './index.css';
 import VitessceGrid from '../../src';
 // If you've installed from NPM, use "from 'vitessce-grid'" instead.
 
+import layout from './layout.json';
+
 const handleClass = 'demo-handle';
 
 function Demo(props) {
@@ -29,42 +31,9 @@ function getComponent(name) {
 }
 
 export default function renderDemo(id) {
-  const responsiveLayout = {
-    columns: {
-      // First two columns are equal,
-      // third column is constant;
-      // Grid cell width stays roughly constant,
-      // but more columns are available in a wider window.
-      1400: [0, 6, 12, 14],
-      1200: [0, 5, 10, 12],
-      1000: [0, 4, 8, 10],
-      800: [0, 3, 6, 8],
-      600: [0, 2, 4, 8],
-    },
-    layout: [
-      /* eslint-disable object-curly-newline */
-      /* eslint-disable object-property-newline */
-      { component: 'Demo',
-        props: { text: 'header' },
-        x: 0, y: 0, w: 2 },
-      { component: 'Demo',
-        props: { text: 'body, left' },
-        x: 0, y: 1, h: 2 },
-      { component: 'Demo',
-        props: { text: 'body, right' },
-        x: 1, y: 1, h: 2 },
-      { component: 'Demo',
-        props: { text: 'sidebar' },
-        x: 2, y: 0, h: 3 },
-      { component: 'Demo',
-        props: { text: 'footer' },
-        x: 0, y: 3, w: 3 },
-      /* eslint-enable */
-    ],
-  };
   ReactDOM.render(
     <VitessceGrid
-      layout={responsiveLayout}
+      layout={layout}
       getComponent={getComponent}
       draggableHandle={`.${handleClass}`}
       padding={50}
