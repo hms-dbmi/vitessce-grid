178d7868cea919d0e9eab74ac45d5befee0d50f5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-05-15 12:52:24 -0400

    Some enzyme tests

--- src/VitessceGrid.test.js
+++ src/VitessceGrid.test.js
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { resolveLayout, VitessceGrid } from './VitessceGrid';
@@ -91,29 +91,55 @@ describe('VitessceGrid.js', () => {
   });
 
   describe('<VitessceGrid />', () => {
-    it('works', () => {
-      function FakeComponent() {}
-      /* eslint-disable object-curly-newline */
-      /* eslint-disable object-property-newline */
-      const layoutJson = {
-        columns: {
-          600: [0, 2, 4, 8],
-        },
-        components: [
-          { component: 'FakeComponent',
-            props: { text: 'header' },
-            x: 0, y: 0, w: 2 },
-        ],
-      };
-      /* eslint-enable */
-
+    function FakeComponent(props) {
+      return <span>{props.text}</span>;
+    }
+    /* eslint-disable object-curly-newline */
+    /* eslint-disable object-property-newline */
+    const layoutJson = {
+      columns: {
+        600: [0, 2, 4, 8],
+      },
+      components: [
+        { component: 'FakeComponent',
+          props: { text: 'Hello World' },
+          x: 0, y: 0, w: 2 },
+      ],
+    };
+    /* eslint-enable */
+    it('shallow() works', () => {
       const wrapper = shallow(<VitessceGrid
         layout={layoutJson}
         getComponent={() => FakeComponent}
-        draggableHandle=".fakeClass"
+        draggableHandle=".my-handle"
       />);
 
       expect(wrapper.find('div').length).toEqual(1);
+      expect(wrapper.find('div').text()).toEqual('<FakeComponent />');
+
+      expect(wrapper.find('span').length).toEqual(0);
+
+      const style = wrapper.find('style');
+      expect(style.length).toEqual(1);
+      expect(style.text()).toContain('.my-handle {');
+      expect(style.text()).toContain('.my-handle:active {');
+    });
+
+    it('render() works', () => {
+      const wrapper = render(<VitessceGrid
+        layout={layoutJson}
+        getComponent={() => FakeComponent}
+        draggableHandle=".my-handle"
+      />);
+
+      expect(wrapper.find('div').length).toEqual(1);
+      expect(wrapper.find('div').text()).toEqual('Hello World');
+
+      expect(wrapper.find('span').length).toEqual(2);
+
+      const style = wrapper.find('style');
+      expect(style.length).toEqual(0);
+      // TODO: Why does render() not generate style?
     });
   });
 });
