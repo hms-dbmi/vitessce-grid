87b82f4ae1a076e4b1d055ead32892274ec69a9f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-05-20 11:21:20 -0400

    Move resolveLayout to utils

--- src/VitessceGrid.js
+++ src/VitessceGrid.js
@@ -2,51 +2,8 @@ import React from 'react';
 
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
-import { makeGridLayout, range, getMaxRows } from './layoutUtils';
+import { getMaxRows, resolveLayout } from './layoutUtils';
 
-
-export function resolveLayout(layout) {
-  const cols = {};
-  const layouts = {};
-  const breakpoints = {};
-  const components = {};
-  const positions = {};
-
-  (('components' in layout) ? layout.components : layout).forEach(
-    (def) => {
-      const id = `r${def.x}_c${def.y}`;
-      components[id] = {
-        component: def.component, props: def.props || {},
-      };
-      positions[id] = {
-        id, x: def.x, y: def.y, w: def.w, h: def.h,
-      };
-    },
-  );
-
-  if ('components' in layout) {
-    Object.entries(layout.columns).forEach(
-      ([width, columnXs]) => {
-        cols[width] = columnXs[columnXs.length - 1];
-        layouts[width] = makeGridLayout(columnXs, positions);
-        breakpoints[width] = width;
-      },
-    );
-  } else {
-    // static layout
-    const id = 'ID';
-    const columnCount = 12;
-    cols[id] = columnCount;
-    layouts[id] = makeGridLayout(range(columnCount + 1), positions);
-    breakpoints[id] = 1000;
-    // Default has different numbers of columns at different widths,
-    // so we do need to override that to ensure the same number of columns,
-    // regardless of window width.
-  }
-  return {
-    cols, layouts, breakpoints, components,
-  };
-}
 
 export function VitessceGrid(props) {
   const {
--- src/layoutUtils.test.js
+++ src/layoutUtils.test.js
@@ -1,6 +1,8 @@
 import expect from 'expect';
 
-import { makeGridLayout, range, getMaxRows } from './layoutUtils';
+import {
+  makeGridLayout, range, getMaxRows, resolveLayout,
+} from './layoutUtils';
 
 describe('layoutUtils.js', () => {
   describe('makeGridLayout', () => {
@@ -78,6 +80,88 @@ describe('layoutUtils.js', () => {
       };
       const gridLayout = makeGridLayout(columnXs, layout);
       expect(getMaxRows([gridLayout])).toEqual(3);
+    });
+  });
+
+  describe('resolveLayout', () => {
+    const componentsSpec = [
+      { component: 'NoProps', x: 0, y: 0 },
+      {
+        component: 'HasProps', props: { foo: 'bar' }, x: 1, y: 1, w: 1, h: 1,
+      },
+    ];
+    const expectedComponents = {
+      r0_c0: {
+        component: 'NoProps',
+        props: {},
+      },
+      r1_c1: {
+        component: 'HasProps',
+        props: {
+          foo: 'bar',
+        },
+      },
+    };
+
+    it('handles responsive', () => {
+      const {
+        cols, layouts, breakpoints, components,
+      } = resolveLayout({
+        columns: {
+          1000: [0, 3, 9, 12],
+          800: [0, 4, 8, 12],
+        },
+        components: componentsSpec,
+      });
+      expect(cols).toEqual({ 800: 12, 1000: 12 });
+      expect(layouts).toEqual(
+        {
+          800: [
+            {
+              h: 1, i: 'r0_c0', w: 4, x: 0, y: 0,
+            },
+            {
+              h: 1, i: 'r1_c1', w: 4, x: 4, y: 1,
+            },
+          ],
+          1000: [
+            {
+              h: 1, i: 'r0_c0', w: 3, x: 0, y: 0,
+            },
+            {
+              h: 1, i: 'r1_c1', w: 6, x: 3, y: 1,
+            },
+          ],
+        },
+      );
+      expect(breakpoints).toEqual({
+        800: '800',
+        1000: '1000',
+      });
+      expect(components).toEqual(expectedComponents);
+    });
+
+    it('handles static', () => {
+      const {
+        cols, layouts, breakpoints, components,
+      } = resolveLayout(
+        componentsSpec,
+      );
+      expect(cols).toEqual({ ID: 12 });
+      expect(layouts).toEqual(
+        {
+          ID: [
+            {
+              h: 1, i: 'r0_c0', w: 1, x: 0, y: 0,
+            },
+            {
+              h: 1, i: 'r1_c1', w: 1, x: 1, y: 1,
+            },
+          ],
+        },
+      );
+      expect(breakpoints).toEqual({ ID: 1000 });
+      expect(components).toEqual(expectedComponents);
     });
   });
 });
