ca07f7cea64d3fc30bc343eb5f542ac2518d3df3
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-05-10 23:27:25 -0400

    Working library bundling

--- rollup.config.lib.js
+++ rollup.config.lib.js
@@ -6,41 +6,59 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
-import html from '@rollup/plugin-html';
-import replace from '@rollup/plugin-replace';
-import scss from 'rollup-plugin-scss';
-
-// Dev server
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
-import { htmlFromTemplate } from './rollup.utils';
+import { terser } from 'rollup-plugin-terser';
 
 const { join } = path;
 
 // Constants for output files:
-const DEMO_DIR = 'demo';
-const DEMO_SRC_DIR = join(DEMO_DIR, 'src');
-const DEMO_DIST_DIR = join(DEMO_DIR, 'dist');
+const SRC_DIR = 'src';
+const LIB_DIR = 'lib';
+const ES_DIR = 'es';
+const UMD_DIR = 'umd';
 const INPUT_JS = 'index.js';
-const OUTPUT_JS = 'demo.js';
-const OUTPUT_CSS = 'demo.css';
+
+const ES_OUTPUT_JS = join(ES_DIR, 'index.js');
+const ES_OUTPUT_MIN_JS = join(ES_DIR, 'index.min.js');
+const LIB_OUTPUT_JS = join(LIB_DIR, 'index.js');
+const LIB_OUTPUT_MIN_JS = join(LIB_DIR, 'index.min.js');
+const UMD_OUTPUT_JS = join(UMD_DIR, 'vitessce-grid.js');
+const UMD_OUTPUT_MIN_JS = join(UMD_DIR, 'vitessce-grid.min.js');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const outputBase = {
+    sourcemap: true,
+    globals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+    }
+};
 
 export default {
-    input: join(DEMO_SRC_DIR, INPUT_JS),
-    output: {
-        file: join(DEMO_DIST_DIR, OUTPUT_JS),
-        sourcemap: true,
-        format: 'umd',
-    },
+    input: join(SRC_DIR, INPUT_JS),
+    output: [
+        {
+            format: 'es',
+            file: (isProduction ? ES_OUTPUT_MIN_JS : ES_OUTPUT_JS),
+            ...outputBase
+        },
+        {
+            format: 'cjs',
+            file: (isProduction ? LIB_OUTPUT_MIN_JS : LIB_OUTPUT_JS),
+            ...outputBase
+        },
+        {
+            name: pkg.name,
+            format: 'umd',
+            file: (isProduction ? UMD_OUTPUT_MIN_JS : UMD_OUTPUT_JS),
+            ...outputBase
+        }
+    ],
     plugins: [
         // Tell Rollup how to resolve packages in node_modules.
         // Reference: https://github.com/rollup/plugins/tree/master/packages/commonjs#using-with-rollupplugin-node-resolve
         resolve({
             browser: true,
-        }),
-        // Tell Rollup how to handle CSS imports.
-        scss({
-            output: join(DEMO_DIST_DIR, OUTPUT_CSS),
         }),
         // Tell Rollup how to handle JSON imports.
         json(),
@@ -68,28 +86,10 @@ export default {
             // Reference: https://github.com/rollup/plugins/tree/master/packages/babel#extensions
             exclude: 'node_modules/**'
         }),
-        replace({
-            // React uses process.env to determine whether a development or production environment.
-            // Reference: https://github.com/rollup/rollup/issues/487#issuecomment-177596512
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        // We want Rollup to generate an HTML file for the demo.
-        // Note: The default output filename is 'index.html'.
-        html({
-            // The following option provides
-            title: `${pkg.name} demo`,
-            // Our demo expects to find the element <div id="demo"></div>.
-            // We need to use a custom templating function to override the default HTML output.
-            // Reference: https://github.com/rollup/plugins/tree/master/packages/html#template
-            template: htmlFromTemplate,
-        }),
-        serve({
-            port: 8000,
-            contentBase: DEMO_DIST_DIR
-        }),
-        livereload(DEMO_DIST_DIR)
+        ...(isProduction ? [
+            terser()
+        ] : [])
     ],
-    // We do not want to declare any externals.
-    // The demo needs React and ReactDOM even though these are externals for the library output.
-    external: []
+    // We do not to inclue React or ReactDOM in the bundle.
+    external: ['react', 'react-dom']
 };
