eaffe87dd4437f5d8e49a4a93b66f1cede95d7ab
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-15 09:19:08 -0400

    Reducer

--- demo/src/renderDemo.js
+++ demo/src/renderDemo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 
 import 'react-grid-layout/css/styles.css';
